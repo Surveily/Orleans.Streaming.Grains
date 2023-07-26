d5ef9c29a35ff279df3da5c9fb0b89a994fa133e
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-07-26 14:22:02 +0000

    clean code

--- src/Orleans.Streaming.Grains.Tests/Streams/Grains/CompoundReceiverGrain.cs
+++ src/Orleans.Streaming.Grains.Tests/Streams/Grains/CompoundReceiverGrain.cs
@@ -15,6 +15,8 @@ namespace Orleans.Streaming.Grains.Tests.Streams.Grains
     [ImplicitStreamSubscription(nameof(CompoundMessage))]
     public class CompoundReceiverGrain : Grain, IBlobReceiverGrain
     {
+        private IAsyncStream<BlobMessage> _blobStream;
+        private IAsyncStream<SimpleMessage> _simpleStream;
         private StreamSubscriptionHandle<CompoundMessage> _subscription;
 
         public override async Task OnActivateAsync(CancellationToken cancellationToken)
@@ -23,20 +25,20 @@ namespace Orleans.Streaming.Grains.Tests.Streams.Grains
             var stream = StreamFactory.Create<CompoundMessage>(streamProvider, this.GetPrimaryKey());
 
             _subscription = await stream.SubscribeAsync(OnNextAsync);
+            _blobStream = StreamFactory.Create<BlobMessage>(streamProvider, this.GetPrimaryKey());
+            _simpleStream = StreamFactory.Create<SimpleMessage>(streamProvider, this.GetPrimaryKey());
+
+            await base.OnActivateAsync(cancellationToken);
         }
 
         private async Task OnNextAsync(CompoundMessage message, StreamSequenceToken token)
         {
-            var streamProvider = this.GetStreamProvider("Default");
-            var blobStream = StreamFactory.Create<BlobMessage>(streamProvider, this.GetPrimaryKey());
-            var simpleStream = StreamFactory.Create<SimpleMessage>(streamProvider, this.GetPrimaryKey());
-
-            await blobStream.OnNextAsync(new BlobMessage
+            await _blobStream.OnNextAsync(new BlobMessage
             {
                 Data = message.Data,
             });
 
-            await simpleStream.OnNextAsync(new SimpleMessage
+            await _simpleStream.OnNextAsync(new SimpleMessage
             {
                 Text = message.Text,
             });
