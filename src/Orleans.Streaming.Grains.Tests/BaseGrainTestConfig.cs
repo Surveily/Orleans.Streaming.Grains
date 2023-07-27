b6b87048b320e45335d6e25be7c8cd080614b2d8
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-07-27 06:53:01 +0000

    try more queues

--- src/Orleans.Streaming.Grains.Tests/BaseGrainTestConfig.cs
+++ src/Orleans.Streaming.Grains.Tests/BaseGrainTestConfig.cs
@@ -44,7 +44,7 @@ namespace Orleans.Streaming.Grains.Test
                            });
                            config.Configure<HashRingStreamQueueMapperOptions>(options =>
                            {
-                               options.Configure(x => x.TotalQueueCount = 1);
+                               options.Configure(x => x.TotalQueueCount = 100);
                            });
                        });
         }
