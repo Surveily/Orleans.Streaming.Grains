01751d32f62bb3282a3da2df59cbdab7feb8ff3a
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-10-31 15:50:50 +0000

    rm unused

--- src/Orleans.Streaming.Grains.Performance/Configs/BasicGrainTestConfig.cs
+++ src/Orleans.Streaming.Grains.Performance/Configs/BasicGrainTestConfig.cs
@@ -4,7 +4,6 @@
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Moq;
 using Orleans.Hosting;
 using Orleans.Streaming.Grains.Extensions;
