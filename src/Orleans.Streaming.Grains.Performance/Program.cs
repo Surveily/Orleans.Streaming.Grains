d3136f0167d9b56dfc02675f4cf60c547b884d4c
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-11-08 09:33:28 +0000

    add stubs for better summaries

--- src/Orleans.Streaming.Grains.Performance/Program.cs
+++ src/Orleans.Streaming.Grains.Performance/Program.cs
@@ -16,5 +16,34 @@ namespace Orleans.Streaming.Grains.Performance
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
                              .Run(args, config);
         }
+
+        /*
+        public static void Main(string[] args)
+        {
+            var config = new ManualConfig
+            {
+                UnionRule = ConfigUnionRule.AlwaysUseGlobal
+            };
+
+            config.AddJob(DefaultConfig.Instance.GetJobs().ToArray());
+            config.AddExporter(DefaultConfig.Instance.GetExporters().ToArray());
+            config.AddAnalyser(DefaultConfig.Instance.GetAnalysers().ToArray());
+            config.AddDiagnoser(DefaultConfig.Instance.GetDiagnosers().ToArray());
+            config.AddValidator(DefaultConfig.Instance.GetValidators().ToArray());
+            config.AddColumnProvider(DefaultConfig.Instance.GetColumnProviders().ToArray());
+
+            var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
+                                             .Run(args, config);
+
+            foreach (var summary in summaries)
+            {
+                MarkdownExporter.Console.ExportToLog(summary, ConsoleLogger.Default);
+                ConclusionHelper.Print(ConsoleLogger.Default, config.GetAnalysers()
+                                                                    .First()
+                                                                    .Analyse(summary)
+                                                                    .ToList());
+            }
+        }
+        */
     }
 }
