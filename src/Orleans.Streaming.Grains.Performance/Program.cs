1b805857ad5674d69c2136f678d468c4d9598f79
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-11-08 11:23:20 +0000

    import better printing

--- src/Orleans.Streaming.Grains.Performance/Program.cs
+++ src/Orleans.Streaming.Grains.Performance/Program.cs
@@ -2,7 +2,10 @@
 // Copyright (c) Surveily Sp. z o.o.. All rights reserved.
 // </copyright>
 
+using BenchmarkDotNet.Analysers;
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Running;
 
 namespace Orleans.Streaming.Grains.Performance
@@ -11,10 +14,45 @@ namespace Orleans.Streaming.Grains.Performance
     {
         public static void Main(string[] args)
         {
-            var config = DefaultConfig.Instance;
+#if DEBUG
+            RunVerbose(args);
+#else
+            RunSummaries(args);
+#endif
+        }
+
+        private static void RunVerbose(string[] args)
+        {
+            var config = DefaultConfig.Instance
+                                      .WithOptions(ConfigOptions.DisableOptimizationsValidator);
 
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
                              .Run(args, config);
+        }
+
+        private static void RunSummaries(string[] args)
+        {
+            var config = new ManualConfig
+            {
+                UnionRule = ConfigUnionRule.AlwaysUseGlobal
+            };
+
+            config.AddJob(DefaultConfig.Instance.GetJobs().ToArray());
+            config.AddExporter(DefaultConfig.Instance.GetExporters().ToArray());
+            config.AddAnalyser(DefaultConfig.Instance.GetAnalysers().ToArray());
+            config.AddDiagnoser(DefaultConfig.Instance.GetDiagnosers().ToArray());
+            config.AddValidator(DefaultConfig.Instance.GetValidators().ToArray());
+            config.AddColumnProvider(DefaultConfig.Instance.GetColumnProviders().ToArray());
+
+            var logger = ConsoleLogger.Default;
+            var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
+                                             .Run(args, config);
+
+            foreach (var summary in summaries)
+            {
+                MarkdownExporter.Console.ExportToLog(summary, logger);
+                ConclusionHelper.Print(logger, summary.BenchmarksCases.First().Config.GetCompositeAnalyser().Analyse(summary).ToList());
+            }
         }
     }
 }
